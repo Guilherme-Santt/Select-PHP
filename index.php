db15bb83e9ff0371eb2bf630b02ecd3b247a7fce
Guilherme-Santt <guisant@icloud.com>
2024-01-30 10:19:21 -0300

    conexão php + verificação campo select estado

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+include('conexao.php');
+$sql_code = "SELECT * FROM estados ORDER BY nome ASC";
+$query = $mysqli->query($sql_code) or die($mysqli->error);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="GET">
+        <select name="estado" required>
+            <option value="">Selecione um estado</option>
+            <?php 
+            while($estado = $query->fetch_assoc()){?>
+            <option 
+            <?php if(isset($_GET['estado']) && ($_GET['estado']) == $estado['id']) echo "selected"  ?>
+            value="<?php echo $estado['id']?>"><?php echo $estado['nome']?></option>
+            <?php } ?>
+        </select>
+        <?php if(isset($_GET['estado'])){ ?>
+        <select required name="cidade">
+
+        </select>
+        <?php } ?>
+        <button type="submit">Avançar</button>
+    </form>
+</body>
+</html>
